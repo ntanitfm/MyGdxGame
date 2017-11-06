81f3fabda960994504471a8e9fe0fa5fb7b0d8f2
ntani <ntani@techfirm.co.jp>
2017-11-06 20:14:14 +0900

    interface実装例(コミット忘れ)

---
+++ android/src/com/mygdx/game/FireBaseOperator.java
@@ -0,0 +1,20 @@
+package com.mygdx.game;
+
+import com.mygdx.game.item.ResultData;
+
+import java.util.List;
+
+/**
+ * Created by ntani on 2017/11/06.
+ */
+
+public class FireBaseOperator implements DbOperator {
+    @Override
+    public void post(ResultData res) {
+
+    }
+    @Override
+    public List<ResultData> get() {
+        return null;
+    }
+}
