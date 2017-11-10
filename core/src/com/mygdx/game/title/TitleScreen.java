27cbe463642fabb765f90b9e5032b61263909ffe
ntani <ntani@techfirm.co.jp>
2017-11-10 19:25:17 +0900

    一時コミットその２

--- core/src/com/mygdx/game/title/TitleScreen.java
+++ core/src/com/mygdx/game/title/TitleScreen.java
@@ -3,11 +3,16 @@ package com.mygdx.game.title;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Timer;
 import com.mygdx.game.item.Config;
 import com.mygdx.game.license.LicenseScreen;
 import com.mygdx.game.main.MyGdxGame;
@@ -46,7 +51,6 @@ public class TitleScreen extends ScreenAdapter {
         rankingButton = env.getTitleTextButton(Config.RANK, 370);
         // ライセンス表示ボタン
         info = env.getInfoButton();
-        license = env.getLicense();
     }
 
     // 部品登録
@@ -62,6 +66,42 @@ public class TitleScreen extends ScreenAdapter {
         stage.addActor(normalButton);
         stage.addActor(rankingButton);
         stage.addActor(info);
+
+        license = new Dialog("License", Config.skin) {
+            @Override
+            public float getPrefWidth() {
+                // force dialog width
+                // return Gdx.graphics.getWidth() / 2;
+                return 700f;
+            }
+
+            @Override
+            public float getPrefHeight() {
+                // force dialog height
+                // return Gdx.graphics.getWidth() / 2;
+                return 400f;
+            }
+        };
+        license.setModal(true);
+        license.setMovable(false);
+        license.setResizable(false);
+
+        TextButton btn = new TextButton("OK", Config.skin);
+        btn.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y,
+                                     int pointer, int button) {
+                // Do whatever here for exit button
+                license.hide();
+                license.cancel();
+                license.remove();
+                return true;
+            }
+        });
+        license.show(stage).setPosition(
+                Config.SCRN_WIDTH_CTR - 350,
+                Config.SCRN_HEIGHT_CTR - 200
+        );
         stage.addActor(license);
     }
 
@@ -97,7 +137,6 @@ public class TitleScreen extends ScreenAdapter {
         stage.draw();
         if(env.GAMEMODE.equals(Config.LICE)) {
             Gdx.app.log(TAG, "license");
-            license.show(stage);
         }
         Config.batcher.end();
     }
